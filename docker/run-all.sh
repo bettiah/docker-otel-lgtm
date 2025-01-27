3a5e92aaa97f58bfe952970bf2ea9faf77bd781c
AK <bettiah@gmail.com>
2025-01-27 21:00:57 +0000

    wait for collector

--- docker/run-all.sh
+++ docker/run-all.sh
@@ -25,17 +25,15 @@ wait_ready "Tempo" "http://localhost:3200/ready"
 
 # we query the otelcol_process_uptime_total metric instead, which checks if the collector is up,
 # and indirectly checks if the prometheus endpoint is up.
-#while ! curl -sg 'http://localhost:9090/api/v1/query?query=otelcol_process_uptime_total{}' | jq -r .data.result[0].value[1] | grep '[0-9]' >/dev/null; do
-#	echo "Waiting for the OpenTelemetry collector to start up..."
-#	sleep 1
-#done
+while ! curl -sg 'http://localhost:9090/api/v1/query?query=otelcol_process_uptime_total{}' | jq -r .data.result[0].value[1] | grep '[0-9]' >/dev/null; do
+	echo "Waiting for the OpenTelemetry collector to start up..."
+	sleep 1
+done
 
 touch /tmp/ready
-echo "The Grafana LGTM stack are up and running. (created /tmp/ready)"
+echo "The Grafana LGTM stack is up and running. (created /tmp/ready)"
 
 echo "Open ports:"
-echo " - 4317: OpenTelemetry GRPC endpoint"
-echo " - 4318: OpenTelemetry HTTP endpoint"
 echo " - 3000: Grafana. User: admin, password: admin"
 
 sleep infinity
